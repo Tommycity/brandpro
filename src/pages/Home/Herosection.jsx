792edb9514d92e087b7bb7aeb7ab0d2438462aa3
HapothecaryJosh <tommycity10@outlook.com>
2026-05-25 13:18:56 +0100

    the project size removed

--- src/pages/Home/Herosection.jsx
+++ src/pages/Home/Herosection.jsx
@@ -34,7 +34,7 @@ export const Herosection = () => {
         <div className="flex gap-8 lg:gap-20">
           <div className="flex flex-col text-[#c74e45]">
             <span className="text-[19px] md:text-[40px] lg:text-[40px] font-bold">
-              10000+
+              +
             </span>
             <span className="text-[19px] md:text-[30px] lg:text-[40px] font-bold">
               Projects
